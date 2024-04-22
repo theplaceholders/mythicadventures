c24b6bb1cb198e4f3293030f862b03b74822abeb
Beasted179 <clarios179@gmail.com>
2024-04-22 15:53:17 -0700

    modular

---
+++ src/phaser-game/maps/GameMap.js
@@ -0,0 +1,66 @@
+import Phaser from 'phaser';
+import plantsSheet from '../sprites/TX Plant.png';
+import grassSheet from '../sprites/TX Tileset Grass.png';
+import testMap from './testMap.json';
+
+
+
+export default class GameMap {
+    constructor(scene) {
+        this.scene = scene;
+        this.map = null;
+    }
+
+    preload() {
+        this.scene.load.image("grass", grassSheet);
+        this.scene.load.image("plants", plantsSheet);
+        this.scene.load.tilemapTiledJSON("testMap", testMap);
+    }
+
+    create() {
+        this.map = this.scene.add.tilemap("testMap");
+        const camera = this.scene.cameras.main;
+        
+        // Set the camera to the bounds of the map
+        camera.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+        camera.setZoom(1.5);
+
+        const landscapeTiles = this.map.addTilesetImage("Landscapes", "grass");
+        const plantTiles = this.map.addTilesetImage("Plants", "plants");
+
+        const grassLayer = this.map.createLayer("Bottom Layer ", landscapeTiles, 0, 0);
+        const plantsLayer = this.map.createLayer("Tree Layer", plantTiles, 0, 0);
+
+        // Debug graphics and depth settings
+        grassLayer.setDepth(1);
+        plantsLayer.setDepth(500);
+        plantsLayer.setCollisionByProperty({ collide: true });
+
+        this.scene.physics.world.createDebugGraphic();
+        this.scene.physics.world.drawDebug = true;
+
+        // Render debug graphics to visualize collision tiles
+        plantsLayer.renderDebug(this.scene.add.graphics(), {
+            tileColor: null, // Non-colliding tiles
+            collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
+            faceColor: new Phaser.Display.Color(40, 39, 37, 255)
+        });
+
+        console.log(this.map);  // Output the map object to the console for debugging
+
+        return { grassLayer, plantsLayer };
+    }
+
+    getScene() {
+        return this.scene;
+    }
+
+    getMapDimensions() {
+        if (this.map) {
+            return { width: this.map.widthInPixels, height: this.map.heightInPixels };
+        }
+        return { width: 0, height: 0 }; // Default or error handling
+    }
+}
+
+
